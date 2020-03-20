217143bfca0320a6eb7215d05e024614a4f26e7d
srossbyers <srossbyers@shastaqa.com>
2020-03-20 13:13:22 -0700

    Updated highlight utility to scroll an element into view if required
    Updated element test spec for Contact page mappings
    
    Updated Homepage spec to remove reference to old Homepage class
    
    Created Contact page class and mapped required elements for tests

--- test/specs/sqaHomepage.spec.ts
+++ test/specs/sqaHomepage.spec.ts
@@ -1,11 +1,11 @@
 import {expect} from 'chai';
-import Homepage from 'src/pages/SQA/Homepage';
+import DesktopHomepage from 'src/pages/SQA/DesktopHomepage';
 import Helpers from 'src/utilities/Helpers';
 
 describe('Shasta QA ' + (browser.config as any).view + ' homepage', () => {
     it('loads successfully', () => {
         Helpers.resizeWindow();
         browser.url('');
-        expect(Homepage.sqaLogo.isDisplayed()).to.be.true;
+        expect(DesktopHomepage.UI.Header.sqaLogo.isDisplayed()).to.be.true;
     });
 });
