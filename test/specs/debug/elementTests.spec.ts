7dc639da887ef03c6711dc6ce4c0fd927c403b4c
srossbyers <srossbyers@shastaqa.com>
2020-03-20 17:35:56 -0700

    Added script for running element tests
    Renamed nested markets link
    
    Updated highlight, removed hoverDisplay, and added tryClick and calls to waitForElementToLoad
    
    Refactored all element tests to run in different sections
    
    Added all tests for Header and Nav Bar links
    
    Cleaned up specs config

--- test/specs/debug/elementTests.spec.ts
+++ test/specs/debug/elementTests.spec.ts
@@ -4,62 +4,69 @@ import DesktopContactPage from 'src/pages/SQA/DesktopContactpage';
 import DesktopManagementPage from 'src/pages/SQA/DesktopManagementpage';
 import Helpers from 'src/utilities/Helpers';
 
-describe('All mapped ' + (browser.config as any).view + ' elements', () => {
+describe('All mapped ' + (browser.config as any).view + 'Homepage elements', () => {
     it('can be successfully highlighted', () => {
         Helpers.resizeWindow();
-        // browser.url('');
-        // browser.url('/contact');
-        browser.url('/about/management-team');
+        browser.url('');
 
-        // console.log("Highlighting all Header Elements");
-        // DesktopHomepage.UI.Header.getAllElements().forEach(function(element) {
-        //     Helpers.highlight(element, 'yellow');
-        // });
+        console.log("Highlighting all Header Elements");
+        DesktopHomepage.UI.Header.getAllElements().forEach(function (element) {
+            Helpers.highlight(element, 'yellow');
+        });
 
-        // console.log("Highlighting all NavBar Elements");
-        // DesktopHomepage.UI.NavBar.getAllElements().forEach(function(element) {
-        //     Helpers.highlight(element, 'blue');
-        // });
+        console.log("Highlighting all NavBar Elements");
+        DesktopHomepage.UI.NavBar.getAllElements().forEach(function (element) {
+            Helpers.highlight(element, 'blue');
+        });
 
-        // console.log("Highlighting all Homepage Elements");
-        // DesktopHomepage.UI.getAllElements().forEach(function(element) {
-        //     Helpers.highlight(element);
-        // });
-
-        //DesktopHomepage.UI.NavBar.testingServicesLink.moveTo();
-        // console.log("Highlighting all NavBar Hover Dropdown Elements");
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.desktopApplicationTestingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.webApplicationTestingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.mobileTestingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.iotTestingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.automationTestingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.regulatoryTestingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.bugScrubbingLink);
-
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.marketsLinkNested);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.techStartupsLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.regulatoryMedicalDevicesLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.ventureCapitalistsLink);
-
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.needsAnalysisLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.projectPlanningLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.projectExecutionLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.projectMasteryLink);
-
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.About.whoWeAreLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.About.ourSourcingLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.About.teamLink);
-        // Helpers.highlight(DesktopHomepage.UI.NavBar.About.careersLink);
-
-        // console.log("Highlighting all Contact Page Elements");
-        // DesktopContactPage.UI.Form.getAllElements().forEach(function(element) {
-        //     Helpers.highlight(element);
-        // });
-
-        console.log("Highlighting all Management Page Elements");
-        DesktopManagementPage.getAllElements().forEach(function(element) {
+        console.log("Highlighting all Homepage Elements");
+        DesktopHomepage.UI.getAllElements().forEach(function (element) {
             Helpers.highlight(element);
         });
+
+        console.log("Highlighting all NavBar Hover Dropdown Elements");
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.desktopApplicationTestingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.webApplicationTestingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.mobileTestingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.iotTestingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.automationTestingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.regulatoryTestingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.TestingServices.bugScrubbingLink);
+
+        Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.marketsLinkNested);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.techStartupsLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.regulatoryMedicalDevicesLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.Markets.ventureCapitalistsLink);
+
+        Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.needsAnalysisLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.projectPlanningLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.projectExecutionLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.SQAProcess.projectMasteryLink);
+
+        Helpers.highlight(DesktopHomepage.UI.NavBar.About.whoWeAreLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.About.ourSourcingLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.About.teamLink);
+        Helpers.highlight(DesktopHomepage.UI.NavBar.About.careersLink);
+    });
+});
+describe('All mapped ' + (browser.config as any).view + 'Contact page elements', () => {
+    it('can be successfully highlighted', () => {
+        browser.url('/contact');
+        console.log("Highlighting all Contact Page Elements");
+        DesktopContactPage.UI.Form.getAllElements().forEach(function (element) {
+            Helpers.highlight(element);
+        });
+    });
+});
+describe('All mapped ' + (browser.config as any).view + 'Management page elements', () => {
+    it('can be successfully highlighted', () => {
+        browser.url('/about/management-team');
+        console.log("Highlighting all Management Page Elements");
+        DesktopManagementPage.getAllElements().forEach(function (element) {
+            Helpers.highlight(element);
+        });
+    });
+    it('can output details for all senior management', () => {
         console.log(Helpers.getManagerStrings(DesktopManagementPage.donSection));
         console.log(Helpers.getManagerStrings(DesktopManagementPage.chrisSection));
         console.log(Helpers.getManagerStrings(DesktopManagementPage.annaSection));
